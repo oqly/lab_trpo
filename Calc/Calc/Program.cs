5decfc12b9ef6f3d356851f01e56df35496e5fba
oqly <oqlasso1@mail.ru>
2022-09-21 11:14:38 +0300

    First Calc

---
+++ Calc/Calc/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Let's Calc! Choose an operator + - * /");
+string oper = Console.ReadLine();
+Console.WriteLine("First");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Second");
+int b = Convert.ToInt32(Console.ReadLine());
+switch (oper)
+{
+    case "*":
+        Console.WriteLine(a*b);
+        break;
+    case "/":
+        Console.WriteLine(a/b);
+        break;
+    case "+":
+        Console.WriteLine(a+b);
+        break;
+    case "-":
+        Console.WriteLine(a-b);
+        break;
+}
